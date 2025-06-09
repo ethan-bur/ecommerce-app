2681f2210ec882c9d4b98a82ff824555f1844435
Ethan Burton <ethan.burton@hotmail.com>
2025-06-09 16:30:01 +1200

    search bar css

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -6,14 +6,14 @@ const SearchBar = () => {
     const {search, setSearch, showSearch, setShowSearch} = useContext(ShopContext);
 
     return showSearch ? (
-    <div>
-        <div>
-            <input type='text' placeholder='Search' className='bg-grey-500'></input>
-            <img onClick={()=>setShowSearch(false)} src={assets.cart_icon}></img>
+    <div className=' text-center border-t border-b border-grey-500 bg-gray-1 00 '>
+        <div className='inline-flex items-center justify-center border border-gray-500 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
+            <input type='text' placeholder='Search' className='outline-none w-full'></input>
+            <img className='inline w-3 cursor-pointer' onClick={()=>setShowSearch(true)} src={assets.search_icon}></img>
         </div>
-        <div>
-        <img onClick={()=>setShowSearch(true)} src={assets.cart_icon}></img>
-        </div>
+        <img className='inline w-3 cursor-pointer' onClick={()=>setShowSearch(false)} src={assets.close_icon}></img>            
+            
+        
 
     </div>
   ): null
